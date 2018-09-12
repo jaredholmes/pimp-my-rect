858fd1fcf36ee9949f88e57ec85c49e275c766f9
Jared Holmes <jared@colgro.com>
2018-09-12 13:41:53 +0200

    Edited index view for ensuring user is logged in and added auth logic

--- pmr_app/views.py
+++ pmr_app/views.py
@@ -1,24 +1,59 @@
 from django.shortcuts import render
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
 from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Rectangle
-from . import serializers
+from . import serializers, forms
 
 # Create your views here.
 def home(request):
     context = {
         'site_title': settings.SITE_TITLE,
     }
-    # if request.user.is_authenticated:
-    #     return render(request, 'pmr_app/app.html')
-    # else:
-    #     return render(request, 'pmr_app/login.html', context=context)
-    return render(request, 'pmr_app/app.html')
+    if request.user.is_authenticated:
+        return render(request, 'pmr_app/app.html')
+    else:
+        return render(request, 'pmr_app/index.html', context=context)
+
+def user_login(request, sign_up=False):
+    if request.method == 'POST':
+        if sign_up:
+            form = forms.UserSignUpForm(request.POST)
+        else:
+            form = forms.UserLoginForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            user_username = request.POST['username']
+            user_password = request.POST['password']
+            if sign_up:
+                newUser = User.objects.create_user(username=user_username, password=user_password)
+
+            user = authenticate(request, username=user_username, password=user_password)
+            print(user)
+            if user is not None:
+                login(request, user)
+            return HttpResponseRedirect('/')
+
+    else:
+        if sign_up:
+            form = forms.UserSignUpForm()
+        else:
+            form = forms.UserLoginForm()
+
+    return render(request, 'pmr_app/login.html', { 'form': form })
+
+def sign_up(request):
+    return user_login(request, sign_up=True)
+
+def log_out(request):
+    logout(request)
+    return HttpResponseRedirect('/')
 
 # REST API Views
 class RectangleListView(generics.ListAPIView):
@@ -38,7 +73,6 @@ class RectangleCreateView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# TODO: Improve returns and error handling
 def rectangle_delete_view(request, rectangle_id):
     try:
         rectangle = Rectangle.objects.get(id=rectangle_id)
@@ -46,4 +80,3 @@ def rectangle_delete_view(request, rectangle_id):
         return HttpResponse('Success')
     except:
         return HttpResponse('Failed')
-        print(request.errors)
