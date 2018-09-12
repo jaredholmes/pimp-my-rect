fd64b2aef46ed010e4128c2024f5f4881f619179
Jared Holmes <jared@colgro.com>
2018-09-12 13:30:08 +0200

    Added urls for log in, sign up and log out

--- pmr_app/urls.py
+++ pmr_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import re_path
+from django.urls import path, re_path
 
 from . import views
 
@@ -8,5 +8,11 @@ urlpatterns = [
     re_path(r'^api/create_rectangle/$', views.RectangleCreateView.as_view(), name='rectangle_create_view'),
     re_path(r'^api/delete_rectangle/(?P<rectangle_id>[0-9]+)/$', views.rectangle_delete_view, name='rectangle_delete_view'),
 
+    # Login Patterns
+    path('login/', views.user_login, name='login'),
+    path('sign-up/', views.sign_up, name='sign_up'),
+    path('log-out/', views.log_out, name='log_out'),
+
+    # All other URLs are directed to the SPA
     re_path(r'^.*$', views.home, name='home'),
 ]
